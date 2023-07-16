d41a78cd4c5c2d3bf56fdbd458a15ed6c59ed4a5
Gyanendra <gyanuchy32@gmail.com>
2023-07-17 00:06:05 +0545

    homepage updated

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,29 @@
 
-function myMenulists(){
-  var menuItem = document.getElementById("tooglemenulsits")
-  if menuItem.style.display === ("none"){
-    menuItem.style.display = "block";
-  }
-}
+  // window.onscroll = function();
   
+  // var header = document.getElementById("myHeader");
+  // var sticky = header.offsetTop;
+  // var hold = header.offsetTop;
+  // var search = document.getElementById("mySearchbar");
+  
+  // function myFunction() {
+  //   if (window.pageYOffset > sticky) {
+  //     header.classList.add("sticky");
+  //     search.classList.add("hold")
+  //   } else {
+  //     header.classList.remove("sticky");
+  //     search.classList.remove("hold")
+  //   }
+  // }
 
-
+    function toggleMenulists() {
+      var menuItem = document.getElementById("toogle-menu-lists");
+      if (menuItem.style.display === "none") {
+          menuItem.style.display = "block";
+      } else {
+        menuItem.style.display = "none";
+      }
+    }
 
 
 
